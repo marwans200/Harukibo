4adcae3696244da2caf074c59a1831461cbc17f7
marwans200 <prounitycoder@gmail.com>
2025-02-23 14:33:24 +0530

    Added Rigidbody stuff

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,17 +3,13 @@ using Fusion;
 
 public class Player : NetworkBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed = 5f;
     public float jumpForce = 5f;
-    public float gravity = 9.81f;
-    private Vector3 _velocity;
-
-    private CharacterController _controller;
+    public Rigidbody rb;
     public Camera _camera;
 
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();
         if (!Object.HasInputAuthority) _camera.gameObject.SetActive(false);
     }
     
@@ -25,24 +21,8 @@ public class Player : NetworkBehaviour
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
-            Vector3 move = new Vector3(h, 0, v).normalized * moveSpeed;
-            _controller.Move(move * Runner.DeltaTime);
-
-            // Apply gravity
-            if (_controller.isGrounded)
-            {
-                _velocity.y = -0.1f; // Small downward force to keep grounded
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    _velocity.y = jumpForce; // Jumping
-                }
-            }
-            else
-            {
-                _velocity.y -= gravity * Runner.DeltaTime; // Apply gravity
-            }
-
-            _controller.Move(_velocity * Runner.DeltaTime);
+            Vector3 force = new Vector3(h * speed, 0, v * speed);
+            rb.AddRelativeForce(force);
         }
     }
 }
